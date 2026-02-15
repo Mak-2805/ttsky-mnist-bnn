1432a6de6371bed25346406dcf0c7d8e5d71ac7b
Mak-2805 <makariossamwail@gmail.com>
2026-02-15 14:12:36 -0500

    Added fsm module

---
+++ src/fsm.sv
@@ -0,0 +1,86 @@
+
+module fsm(
+    input logic clk, // Top level input
+    input logic rst_n, // Top level input
+    input logic mode, // Top level input
+    input logic load_done,
+    input logic layer_1_done,
+    input logic layer_2_done,
+    input logic layer_3_done,
+    output logic [2:0] state
+);
+
+    typedef enum logic [2:0] {
+        s_IDLE    = 3'b000,
+        s_LOAD    = 3'b001,
+        s_LAYER_1 = 3'b010,
+        s_LAYER_2 = 3'b011,
+        s_LAYER_3 = 3'b100
+    } state_t;
+	
+  	// Current state, Next state
+    logic [2:0] cs, ns;
+    
+    always_comb begin
+    	case (cs)
+      	
+        //when we're in idle
+        //only load data when mode is load data and we are not processing 
+      	s_IDLE: begin
+        	if (mode) begin
+          	ns = s_LOAD;
+        	end else begin
+          	ns = cs;
+          end
+        end
+        
+        s_LOAD: begin
+        	if (load_done) begin
+          	ns = s_LAYER_1;
+          end else begin
+          	ns = cs;
+          end
+        end
+        
+        s_LAYER_1: begin
+        	if (layer_1_done) begin
+          	ns = s_LAYER_2;
+          end else begin
+          	ns = cs;
+          end
+        end 
+        
+        s_LAYER_2: begin
+        	if (layer_2_done) begin
+          	ns = s_LAYER_3;
+          end else begin
+          	ns = cs;
+          end
+        end
+        
+        s_LAYER_3: begin
+        	if (layer_3_done) begin
+          	ns = s_IDLE;
+          end else begin
+          	ns = cs;
+          end
+        end
+        
+        default: begin 
+            cs = s_IDLE;
+        end
+      endcase
+    end
+
+    always_ff @(posedge clk) begin
+        if (!rst_n) begin
+             cs <= s_IDLE;
+        end else begin
+             cs <= ns;
+        end
+    end
+
+		assign state = cs;
+
+endmodule
+
--- src/tt_um_mnist_bnn.v
+++ src/tt_um_mnist_bnn.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_mnist_bnn (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,22 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out[7:3]  = 'd0;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 'd0;
+  assign uio_oe  = 'd0;
+
+  fsm top_fsm (
+        .clk(clk),
+        .rst_n(rst_n),
+        .mode(ui_in[0]),
+        .load_done(ui_in[1]),
+        .layer_1_done(ui_in[2]),
+        .layer_2_done(ui_in[3]),
+        .layer_3_done(ui_in[4]),
+        .state(uo_out[2:0])
+    );
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena,ui_in[7:5], 1'b0};
 
 endmodule
